11360195a538026431690dad65a19b078dfb3696
Kirk Lin <linkirk@163.com>
2024-07-02 14:15:40 +0800

    perf: enable the `terser` compression tool in development mode for performance optimization

--- config/dev.ts
+++ config/dev.ts
@@ -5,6 +5,27 @@ export default {
     quiet: false,
     stats: true,
   },
-  mini: {},
+  mini: {
+    webpackChain: (chain) => {
+      chain.merge({
+        plugin: {
+          install: {
+            // eslint-disable-next-line ts/no-require-imports
+            plugin: require("terser-webpack-plugin"),
+            args: [
+              {
+                terserOptions: {
+                  compress: true, // 默认使用terser压缩
+                  // mangle: false,
+                  keep_classnames: true, // 不改变class名称
+                  keep_fnames: true, // 不改变函数名称
+                },
+              },
+            ],
+          },
+        },
+      });
+    },
+  },
   h5: {},
 } satisfies UserConfigExport;
