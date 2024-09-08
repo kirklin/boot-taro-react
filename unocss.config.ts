e6f175b87df4ab7cfcafc928ed93a1f2a7bab53d
Kirk Lin <linkirk@163.com>
2024-09-08 18:45:59 +0800

    feat: add unocss shortcuts

--- unocss.config.ts
+++ unocss.config.ts
@@ -28,6 +28,54 @@ export default defineConfig({
     presetAttributify(),
     presetRemRpx({ mode: isApplet ? "rem2rpx" : "rpx2rem" }),
   ],
+  shortcuts: {
+    // position
+    "common-bg": "bg-gray-100 dark:bg-gray-900",
+    "pr": "relative",
+    "pa": "absolute",
+    "pf": "fixed",
+    "ps": "sticky",
+
+    // position layout
+    "position-x-center": "absolute left-1/2 -translate-x-1/2",
+    "pxc": "position-x-center",
+    "position-y-center": "absolute top-1/2 -translate-y-1/2",
+    "pyc": "position-y-center",
+    "position-center": "position-x-center position-y-center",
+    "pc": "position-center",
+
+    // size
+    "size-0": "w-0 h-0",
+    "size-full": "w-full h-full",
+    "size-screen": "w-screen h-screen",
+    "size-1/2": "w-1/2 h-1/2",
+
+    // flex layout
+    "flex-center": "flex justify-center items-center",
+    "flex-col-center": "flex-center flex-col",
+    "flex-x-center": "flex justify-center",
+    "flex-y-center": "flex items-center",
+  },
+  theme: {
+    colors: {
+      primary: "var(--primary-color)",
+      primary_1: "var(--primary-color-1)",
+      primary_2: "var(--primary-color-2)",
+      primary_3: "var(--primary-color-3)",
+      primary_4: "var(--primary-color-4)",
+      primary_5: "var(--primary-color-5)",
+      primary_6: "var(--primary-color-6)",
+      primary_7: "var(--primary-color-7)",
+      primary_8: "var(--primary-color-8)",
+      primary_9: "var(--primary-color-9)",
+      primary_10: "var(--primary-color10)",
+      info: "var(--info-color)",
+      success: "var(--success-color)",
+      warning: "var(--warning-color))",
+      danger: "var(--danger-color))",
+      active: "var(--active-color))",
+    },
+  },
   transformers: [
     transformerDirectives(),
     transformerVariantGroup(),
