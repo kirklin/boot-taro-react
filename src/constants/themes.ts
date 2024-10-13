0dbc2825dbed17d00db93a0028377b867c7a653e
Kirk Lin <linkirk@163.com>
2024-10-13 15:00:42 +0800

    fix: button primary color

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -2,17 +2,17 @@
 import type { ConfigProviderThemeVars } from "@taroify/core";
 
 export const lightTheme: ConfigProviderThemeVars = {
-  buttonPrimaryBackgroundColor: "#3B82F6FF",
+  buttonPrimaryBackgroundColor: "var(--primary-color)",
   // 添加其他亮色主题变量
 };
 
 export const darkTheme: ConfigProviderThemeVars = {
-  buttonPrimaryBackgroundColor: "#1D4ED8",
+  buttonPrimaryBackgroundColor: "var(--primary-color-7)",
   // 添加其他暗色主题变量
 };
 
 export const greenTheme: ConfigProviderThemeVars = {
-  buttonPrimaryBackgroundColor: "#D6E5BE",
+  buttonPrimaryBackgroundColor: "var(--green)",
   // 添加其他绿色主题变量
 };
 
