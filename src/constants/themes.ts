2846efaa2141dd86a19865824f6cd36ab7f29f78
Kirk Lin <linkirk@163.com>
2024-11-15 12:26:21 +0800

    feat: add tag custom theme

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -3,16 +3,22 @@ import type { ConfigProviderThemeVars } from "@taroify/core";
 
 export const lightTheme: ConfigProviderThemeVars = {
   buttonPrimaryBackgroundColor: "var(--primary-color)",
+  tagDefaultBackgroundColor: "var(--gray-2)",
+  tagDefaultColor: "var(--gray-8)",
+  tagColor: "var(--white)",
+  tabsLineBorderRadius: "32px",
   // 添加其他亮色主题变量
 };
 
 export const darkTheme: ConfigProviderThemeVars = {
   buttonPrimaryBackgroundColor: "var(--primary-color-7)",
+  tabsLineBorderRadius: "32px",
   // 添加其他暗色主题变量
 };
 
 export const greenTheme: ConfigProviderThemeVars = {
   buttonPrimaryBackgroundColor: "var(--green)",
+  tabsLineBorderRadius: "32px",
   // 添加其他绿色主题变量
 };
 
