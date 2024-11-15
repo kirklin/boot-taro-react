01bb405c0f9275567941f5c8961442b05334d81f
Kirk Lin <linkirk@163.com>
2024-11-15 14:46:10 +0800

    feat: add bottom actions

--- src/components/PageWrapper/BottomActions.tsx
+++ src/components/PageWrapper/BottomActions.tsx
@@ -1,12 +1,77 @@
 import { FixedView } from "@taroify/core";
 import { View } from "@tarojs/components";
+import Taro, { useRouter } from "@tarojs/taro";
+import { useEffect, useState } from "react";
+import { PAGES } from "~/constants/routes";
 
-// 底部操作按钮组件
 export default function BottomActions() {
+  const [currentPage, setCurrentPage] = useState("");
+  const router = useRouter();
+
+  // 设置当前页面路径
+  useEffect(() => {
+    setCurrentPage(router.path);
+  }, [router]);
+
+  // 页面跳转处理
+  function handleRedirect(page: string) {
+    setCurrentPage(page);
+    Taro.redirectTo({ url: page });
+  }
+
+  // 检查当前页面是否属于要显示底部导航的页面
+  const showBottomActions = [PAGES.HOME_PAGE, PAGES.PROFILE_PAGE].includes(currentPage);
+
   return (
-    <FixedView position="bottom" safeArea="bottom">
-      <View className="bottom-actions flex items-center justify-between">
-      </View>
-    </FixedView>
+    // 如果当前页面不在允许显示导航的页面列表中，则不渲染导航
+    showBottomActions && (
+      <FixedView position="bottom" safeArea="bottom">
+        <View
+          className="bottom-actions mb-3 flex justify-around rounded-full bg-white px-4 py-2 shadow-lg"
+          style={{ width: "fit-content", margin: "1rem auto" }}
+        >
+          {/* 留言页图标 */}
+          <BottomActionButton
+            className={` ${currentPage === PAGES.HOME_PAGE ? "bg-primary-1 text-primary-6" : "text-text"}`}
+            icon="i-line-md-home-md-alt-twotone"
+            onClick={() => handleRedirect(PAGES.HOME_PAGE)}
+            index={0}
+          />
+          {/* 个人资料页图标 */}
+          <BottomActionButton
+            className={` ${currentPage === PAGES.PROFILE_PAGE ? "bg-primary-1 text-primary-6" : "text-text"}`}
+            icon="i-line-md-person-twotone"
+            onClick={() => handleRedirect(PAGES.PROFILE_PAGE)}
+            index={2}
+          />
+        </View>
+      </FixedView>
+    )
+  );
+}
+
+// 导航按钮组件
+interface BottomActionButtonProps {
+  className: string;
+  icon: string;
+  onClick: () => void;
+  index: number; // index 参数来表示按钮位置
+}
+
+function BottomActionButton({ className, icon, onClick, index }: BottomActionButtonProps) {
+  // 根据 index 动态设置 margin
+  const marginClass = index === 0
+    ? "mr-2" // 第一个按钮只设置右边距
+    : index === 2
+      ? "ml-2" // 最后一个按钮只设置左边距
+      : "mx-2"; // 中间的按钮左右边距都有
+
+  return (
+    <View
+      className={`flex items-center justify-center rounded-full size-10 ${marginClass} ${className}`}
+      onClick={onClick}
+    >
+      <View className={`${icon} text-base`}></View>
+    </View>
   );
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import { Avatar, Tag } from "@taroify/core";
-import { Image, ScrollView, Text, View } from "@tarojs/components";
+import { Image, Text, View } from "@tarojs/components";
 import Taro, { useLoad } from "@tarojs/taro";
 import { useState } from "react";
 import PageWrapper from "~/components/PageWrapper";
@@ -27,117 +27,115 @@ export default function Index() {
       className="h-full"
       shouldShowNavigationMenu={false}
     >
-      <ScrollView scrollY className="h-full">
-        <View className="p-4 space-y-6">
-          {/* 用户信息区域 */}
-          <View className="flex items-center justify-between" onClick={() => redirectTo(ADAPTED_PAGES.PROFILE)}>
-            <View className="flex items-center space-x-2">
-              <View className="wave-hand text-2xl">👋</View>
-              <View className="text-lg text-gray-800 font-medium">你好, 开发者</View>
-            </View>
-            <Avatar src="https://avatars.githubusercontent.com/u/17453452?v=4" size="large" />
+      <View className="p-4 space-y-6">
+        {/* 用户信息区域 */}
+        <View className="flex items-center justify-between" onClick={() => redirectTo(ADAPTED_PAGES.PROFILE)}>
+          <View className="flex items-center space-x-2">
+            <View className="wave-hand text-2xl">👋</View>
+            <View className="text-lg text-gray-800 font-medium">你好, 开发者</View>
           </View>
+          <Avatar src="https://avatars.githubusercontent.com/u/17453452?v=4" size="large" />
+        </View>
 
-          <View className="mb-4 text-lg text-gray-600 font-medium">
-            欢迎使用Boot Taro React模板
-          </View>
+        <View className="mb-4 text-lg text-gray-600 font-medium">
+          欢迎使用Boot Taro React模板
+        </View>
 
-          {/* 功能卡片区域 */}
-          <View className="flex space-x-4">
-            {/* 示例卡片1 */}
-            <View className="flex-1 rounded-2xl bg-primary p-4 shadow-sm" onClick={() => redirectTo(ADAPTED_PAGES.HOME)}>
-              <View className="mb-4 text-lg text-white font-medium">示例卡片1</View>
-              <Avatar.Group>
-                {[1, 2, 3].map(i => (
-                  <Avatar key={i} src={`https://avatars.githubusercontent.com/u/17453452?v=${i}`} />
-                ))}
-              </Avatar.Group>
-              <View className="mt-2 text-sm text-white">
-                这里可以放一些统计信息
-              </View>
-            </View>
-
-            {/* 示例卡片2 */}
-            <View className="flex-1 space-y-4">
-              <View className="rounded-2xl bg-white p-4 shadow-sm">
-                <View className="mb-2 flex items-center justify-between">
-                  <View className="flex items-center">
-                    <View className="mr-2 h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                      <View className="i-mdi-cog text-lg text-purple-500" />
-                    </View>
-                    <Text className="text-base text-gray-800 font-medium">示例开关</Text>
-                  </View>
-                </View>
-                <View className="mb-2 flex justify-center">
-                  <View
-                    className="h-6 w-12 cursor-pointer rounded-full bg-gray-200 p-1"
-                    onClick={toggleSwitch}
-                  >
-                    <View
-                      className="h-4 w-4 transform rounded-full bg-white shadow-md transition-transform duration-300 ease-in-out"
-                      style={{ transform: isToggled ? "translateX(1.5rem)" : "translateX(0)" }}
-                    />
-                  </View>
-                </View>
-                <Text className="text-center text-xs text-gray-600">这是一个示例开关</Text>
-              </View>
-
-              <View className="rounded-2xl bg-white p-4 shadow-sm" onClick={() => redirectTo(ADAPTED_PAGES.HOME)}>
-                <View className="mb-2 flex items-center justify-between">
-                  <View className="flex items-center">
-                    <View className="mr-2 h-8 w-8 flex items-center justify-center rounded-full bg-white">
-                      <View className="i-mdi-card text-lg text-green-500" />
-                    </View>
-                    <Text className="text-base text-gray-800 font-medium">示例功能卡片</Text>
-                  </View>
-                </View>
-                <Text className="text-center text-xs text-gray-600">点击查看示例</Text>
-              </View>
-            </View>
-          </View>
-
-          {/* 示例列表区域 */}
-          <View className="">
-            <View className="mb-4 flex items-center justify-between">
-              <View className="text-lg text-gray-800 font-medium">
-                <Text className="mr-1">示例列表</Text>
-                <Tag color="primary" shape="rounded">
-                  3
-                </Tag>
-              </View>
-              <View
-                className="rounded-full text-sm text-blue-500 font-medium"
-                onClick={() => redirectTo(ADAPTED_PAGES.HOME)}
-              >
-                查看全部
-              </View>
-            </View>
-            <View className="relative m-0 space-y-4">
-              {[1, 2, 3].map((item, index) => (
-                <View
-                  key={item}
-                  className="absolute w-full transition-transform"
-                  style={{
-                    transform: `translateY(${index * 1.6}rem) scale(${1 - index * 0.1})`,
-                    zIndex: 4 - index,
-                  }}
-                >
-                  <View className="flex items-center rounded-xl bg-gray-100 p-4 shadow-lg">
-                    <Image className="mr-2 h-8 w-8 rounded-full" src={`https://avatars.githubusercontent.com/u/17453452?v=" />${item}`} />
-                    <View className="flex-1">
-                      <View className="text-gray-800 font-medium">
-                        示例项目
-                        {item}
-                      </View>
-                      <View className="truncate text-gray-600">这是一个示例项目描述</View>
-                    </View>
-                  </View>
-                </View>
+        {/* 功能卡片区域 */}
+        <View className="flex space-x-4">
+          {/* 示例卡片1 */}
+          <View className="flex-1 rounded-2xl bg-primary-6 p-4 shadow-sm" onClick={() => redirectTo(ADAPTED_PAGES.HOME)}>
+            <View className="mb-4 text-lg text-white font-medium">示例卡片1</View>
+            <Avatar.Group>
+              {[1, 2, 3].map(i => (
+                <Avatar key={i} src={`https://avatars.githubusercontent.com/u/17453452?v=${i}`} />
               ))}
+            </Avatar.Group>
+            <View className="mt-2 text-sm text-white">
+              这里可以放一些统计信息
+            </View>
+          </View>
+
+          {/* 示例卡片2 */}
+          <View className="flex-1 space-y-4">
+            <View className="rounded-2xl bg-white p-4 shadow-sm">
+              <View className="mb-2 flex items-center justify-between">
+                <View className="flex items-center">
+                  <View className="mr-2 h-8 w-8 flex items-center justify-center rounded-full bg-white">
+                    <View className="i-mdi-cog text-lg text-purple-500" />
+                  </View>
+                  <Text className="text-base text-gray-800 font-medium">示例开关</Text>
+                </View>
+              </View>
+              <View className="mb-2 flex justify-center">
+                <View
+                  className="h-6 w-12 cursor-pointer rounded-full bg-gray-200 p-1"
+                  onClick={toggleSwitch}
+                >
+                  <View
+                    className="h-4 w-4 transform rounded-full bg-white shadow-md transition-transform duration-300 ease-in-out"
+                    style={{ transform: isToggled ? "translateX(1.5rem)" : "translateX(0)" }}
+                  />
+                </View>
+              </View>
+              <Text className="text-center text-xs text-gray-600">这是一个示例开关</Text>
+            </View>
+
+            <View className="rounded-2xl bg-white p-4 shadow-sm" onClick={() => redirectTo(ADAPTED_PAGES.HOME)}>
+              <View className="mb-2 flex items-center justify-between">
+                <View className="flex items-center">
+                  <View className="mr-2 h-8 w-8 flex items-center justify-center rounded-full bg-white">
+                    <View className="i-mdi-card text-lg text-green-500" />
+                  </View>
+                  <Text className="text-base text-gray-800 font-medium">示例功能卡片</Text>
+                </View>
+              </View>
+              <Text className="text-center text-xs text-gray-600">点击查看示例</Text>
             </View>
           </View>
         </View>
-      </ScrollView>
+
+        {/* 示例列表区域 */}
+        <View className="">
+          <View className="mb-4 flex items-center justify-between">
+            <View className="text-lg text-gray-800 font-medium">
+              <Text className="mr-1">示例列表</Text>
+              <Tag color="primary" shape="rounded">
+                3
+              </Tag>
+            </View>
+            <View
+              className="rounded-full text-sm text-blue-500 font-medium"
+              onClick={() => redirectTo(ADAPTED_PAGES.HOME)}
+            >
+              查看全部
+            </View>
+          </View>
+          <View className="relative m-0 space-y-4">
+            {[1, 2, 3].map((item, index) => (
+              <View
+                key={item}
+                className="absolute w-full transition-transform"
+                style={{
+                  transform: `translateY(${index * 1.6}rem) scale(${1 - index * 0.1})`,
+                  zIndex: 4 - index,
+                }}
+              >
+                <View className="flex items-center rounded-xl bg-gray-100 p-4 shadow-lg">
+                  <Image className="mr-2 h-8 w-8 rounded-full" src={`https://avatars.githubusercontent.com/u/17453452?v=" />${item}`} />
+                  <View className="flex-1">
+                    <View className="text-gray-800 font-medium">
+                      示例项目
+                      {item}
+                    </View>
+                    <View className="truncate text-gray-600">这是一个示例项目描述</View>
+                  </View>
+                </View>
+              </View>
+            ))}
+          </View>
+        </View>
+      </View>
     </PageWrapper>
   );
 }
