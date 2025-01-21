dc3696d4979fef0655922edf3f89b5f96186b708
Kirk Lin <linkirk@163.com>
2025-01-21 22:04:21 +0800

    feat: add `react-query`

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,17 @@
 import type { PropsWithChildren } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { View } from "@tarojs/components";
 import { nextTick, useDidHide, useDidShow, useLaunch } from "@tarojs/taro";
 import { useLaunchOptions } from "taro-hooks";
 import { cache } from "~/cache";
 import { initSRSDK } from "~/sr.config";
 import { fetchAndCacheSystemInfoAsync, updateVersion } from "~/utils";
+import { queryClient } from "./lib/react-query";
 import "@kirklin/reset-css/taro/kirklin.css";
 import "uno.css";
 import "./app.scss";
+import "event-target-polyfill";
+import "yet-another-abortcontroller-polyfill";
 
 // 是否开启腾讯有数
 const isSDKEnabled = false;
@@ -17,7 +21,7 @@ initSRSDK(isSDKEnabled);
 function App({ children }: PropsWithChildren<any>) {
   const launchOptions = useLaunchOptions();
   useLaunch(() => {
-    cache.set("launchOptions", launchOptions).then(() => console.log("App launched.", launchOptions));
+    cache.set("launchOptions", launchOptions).then(() => console.debug("App launched.", launchOptions));
   });
   // 对应 onShow
   useDidShow(() => {
@@ -31,11 +35,15 @@ function App({ children }: PropsWithChildren<any>) {
 
   // 对应 onHide
   useDidHide(() => {
-    console.log("app hide");
+    console.debug("app hide");
   });
 
   // children 是将要会渲染的页面
-  return <View className="font-chinese antialiased">{children}</View>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <View className="font-chinese antialiased">{children}</View>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
